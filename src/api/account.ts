339158685cc8fa454e19fabeb8dea0d4611882de
Jeffrey Liu <gpugcc@gmail.com>
2024-04-23 09:23:12 +0800

    加入取得環境狀態值

--- src/api/account.ts
+++ src/api/account.ts
@@ -18,14 +18,13 @@ export function InternalLogin({ name, password }) {
  * @returns
  */
 export function InternalRegister({ name, password, email, mobile }) {
-    return Promise.resolve({ name, password, email, mobile });
-    //return service.post('register', {
-    //    account: name,
-    //    name,
-    //    password,
-    //    email,
-    //    mobile
-    //});
+    return service.post('register', {
+        account: name,
+        name,
+        password,
+        email,
+        mobile
+    });
 }
 
 /**
