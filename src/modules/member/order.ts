2e860a32224b853519c0f6ec7f13235df579f0f8
Jeffrey Liu <gpugcc@gmail.com>
2024-05-01 07:04:36 +0800

    調整取得 router 路徑機制

--- src/modules/member/order.ts
+++ src/modules/member/order.ts
@@ -1,7 +1,9 @@
+import { computed } from 'vue';
 import { computedAsync } from '@vueuse/core';
 import { Home as OrderToHome, Authentication } from '@/modules/common';
 import { InternalOrder } from '@/api/point';
 
+const Order = computed(() => '/member/order');
 const Orders = computedAsync(async () => loadOrder());
 
 /**
@@ -11,15 +13,6 @@ const Orders = computedAsync(async () => loadOrder());
 async function loadOrder() {
     const { id } = Authentication();
     return await InternalOrder({ id });
-}
-
-/**
- * 
- * @param member
- * @returns
- */
-function Order() {
-    return '/member/order';
 }
 
 /**
