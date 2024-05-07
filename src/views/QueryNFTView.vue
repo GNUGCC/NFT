64ea50f0009aebd5a708dc080c32a235f647c49f
Jeffrey Liu <gpugcc@gmail.com>
2024-05-07 23:51:59 +0800

    調整未購買 NFT 點數時顯示的外觀

--- src/views/QueryNFTView.vue
+++ src/views/QueryNFTView.vue
@@ -5,39 +5,49 @@
 </script>
 
 <template>
-    <el-card class="card" shadow="hover">
-        <template #header>
-            <template v-if="QueryNFTPoint">
+    <template v-if="QueryNFTPoint">
+        <el-card class="card" shadow="hover">
+            <template #header>
                 <el-alert title="您已購置的 NFT 點數清單" type="success" :closable="false" effect="dark" center show-icon />
             </template>
-            <template v-else>
-                <el-alert title="您沒有購買任何 NFT 點數" type="error" :closable="false" effect="dark" center show-icon />
-            </template>
-        </template>
-        <el-scrollbar height="500px">
-            <el-space wrap>
-                <template v-for="item in QueryNFTPoint" :key="item">
-                    <el-card style="width: 300px; height: 300px" shadow="hover">
-                        <template #header>
-                            <el-row style="justify-content: left">
-                                <el-col :span="24">已使用點數: {{item.lockedPoints}}</el-col>
-                            </el-row>
-                            <el-row>
-                                <el-col :span="24">未使用點數: {{item.availablePoints}}</el-col>
-                            </el-row>
-                        </template>
-                        <el-image :src="item.uri" :fit="'contain'" />
-                    </el-card>                   
-                </template>                
-            </el-space>                
-        </el-scrollbar>        
-        <el-button type="primary" class="info" @click="Home" plain>確定</el-button>
-    </el-card>    
+            <el-scrollbar height="500px">
+                <el-space wrap>
+                    <template v-for="item in QueryNFTPoint" :key="item">
+                        <el-card style="width: 300px; height: 300px" shadow="hover">
+                            <template #header>
+                                <el-row style="justify-content: left">
+                                    <el-col :span="24">已使用點數: {{item.lockedPoints}}</el-col>
+                                </el-row>
+                                <el-row>
+                                    <el-col :span="24">未使用點數: {{item.availablePoints}}</el-col>
+                                </el-row>
+                            </template>
+                            <el-image :src="item.uri" :fit="'contain'" />
+                        </el-card>
+                    </template>
+                </el-space>
+            </el-scrollbar>
+            <el-button type="primary" class="info" @click="Home" plain>確定</el-button>
+        </el-card>
+    </template>
+    <template v-else>
+        <el-card class="notfound" shadow="hover">
+            <template #header>
+                <el-alert title="您沒有購買任何 NFT 點數" type="info" :closable="false" effect="light" center show-icon />
+            </template>            
+            <el-button type="primary" class="info" @click="Home" plain>確定</el-button>
+        </el-card>
+    </template>    
 </template>
 
 <style lang="scss" scoped>
     .querynft {
         width: 100%;
+    }
+
+    .notfound {
+        width: 500px;
+        margin: auto;
     }
 
     .info {
