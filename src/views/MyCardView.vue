4eff85be3ab93fa7b5527e124483ab05336fd72e
Jeffrey Liu <gpugcc@gmail.com>
2024-05-08 20:27:01 +0800

    save

--- src/views/MyCardView.vue
+++ src/views/MyCardView.vue
@@ -13,16 +13,7 @@
     </template>
     <template v-else-if="PayStatus == true">
         <el-alert class="pay" title="系統導向付款頁面中，請稍候…" type="info" :closable="false" effect="light" center show-icon />
-    </template>
-    <!--<template v-else-if="PayStatus == null">
-        <el-card class="card" shadow="hover" width="500px">
-            <template #header>
-                <span class="title">MyCard 訂單訊息</span>
-            </template>
-            <el-alert class="pay" title="系統已完成建立訂單" description="請於付款頁面內完成付款可查詢訂單狀態，再次感謝您的訂購" type="success" :closable="false" effect="light" center show-icon />
-            <el-button type="primary" class="button" @click="Home" round>確定</el-button>
-        </el-card>
-    </template>-->
+    </template>   
     <template v-else-if="PayStatus == false">
         <el-card class="card" shadow="hover">
             <template #header>
