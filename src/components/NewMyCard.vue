27f453b745b03f68e31e3b83997a0d7412ddb1bf
Jeffrey Liu <gpugcc@gmail.com>
2024-05-12 00:04:33 +0800

    save

--- src/components/NewMyCard.vue
+++ src/components/NewMyCard.vue
@@ -4,25 +4,27 @@
 </script>
 
 <template>
-    <el-card class="card" shadow="hover">
-        <template #header>
-            <el-alert title="新增 MyCard 序號" type="info" :closable="false" effect="light" center show-icon />
-        </template>
-        <el-form ref="FormRef"
-                 :model="Form"
-                 :rules="ValidateRules"
-                 status-icon
-                 label-width="1">
-            <el-form-item prop="account">
-                <label class="form-label">MyCard 序號</label>
-                <el-input v-model="Form.account" placeholder="您的MyCard 序號" />
-            </el-form-item>           
-        </el-form>
-        <div style="margin-top: 5px;">
-            <el-button type="success" class="button" @click="AddNewMyCard" plain>儲存</el-button>
-            <el-button type="info" class="button" @click="Console" plain>取消</el-button>
-        </div>
-    </el-card>
+    <div style="margin-top: 150px;">
+        <el-card class="card" shadow="hover">
+            <template #header>
+                <el-alert title="新增 MyCard 序號" type="info" :closable="false" effect="light" center show-icon />
+            </template>
+            <el-form ref="FormRef"
+                     :model="Form"
+                     :rules="ValidateRules"
+                     status-icon
+                     label-width="1">
+                <el-form-item prop="account">
+                    <label class="form-label">MyCard 序號</label>
+                    <el-input v-model="Form.account" placeholder="您的MyCard 序號" />
+                </el-form-item>
+            </el-form>
+            <div style="margin-top: 5px;">
+                <el-button type="success" class="button" @click="AddNewMyCard" plain>儲存</el-button>
+                <el-button type="info" class="button" @click="Console" plain>取消</el-button>
+            </div>
+        </el-card>
+    </div>    
 </template>
 
 <style lang="scss" scoped>
@@ -70,6 +72,7 @@
     .card {
         width: 40%;
         margin: auto;
+        //align-items: center;
         //max-height: 150px !important;
         font-weight: bolder;
         //background-color: blue !important;
