82ff301dc47186a6651a71af73624d3ac742ce92
Jeffrey Liu <gpugcc@gmail.com>
2024-05-14 23:58:15 +0800

    Revert "Merge branch 'feature/入口網站' into dev"
    This reverts commit 99583309a35df99f9d03d2ba038cedccce592a5a, reversing
    changes made to 7f83f1d9a7dccb887913fa2bd135c23c97cd1e5d.

---
+++ src/views/ConsoleView.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+    import Aside from '@/views/Console/AsideView.vue';
+    import Header from '@/views/Console/HeaderView.vue';
+    import Main from '@/views/Console/MainView.vue';
+    import AsideMainView from '@/views/Console/AsideMainView.vue';
+</script>
+
+<template>
+    <el-container class="home" id="menu">
+        <!--<el-aside class="left" width="50px">
+            <AsideMainView />
+        </el-aside>-->
+        <el-aside id="side" width="250px">
+            <Aside />
+        </el-aside>
+        <el-container>
+            <el-header id="header" height="auto"><Header /></el-header>
+            <el-main id="context"><Main /></el-main>
+        </el-container>
+    </el-container>
+</template>
+
+<style lang="scss" scoped>
+    .left {
+        background-color: #1e2a8a;
+    }
+
+    #menu {
+        height: 100vh;
+    }
+
+    #header {
+        background-color: #77bc99;
+    }
+
+    #context {
+        //background-color: #f7f7f7;
+        background-color: gainsboro;
+        background-image: linear-gradient(gainsboro, rgba(255, 1, 0, 0.5));
+        background-repeat: no-repeat;
+        background-attachment: fixed;
+        background-size: cover;
+    }
+
+    #side {
+        background-color: #344a5f;
+    }
+</style>
