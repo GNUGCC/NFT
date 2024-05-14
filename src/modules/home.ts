eb36def2bf21775afeeca71bf990adbfb69379dc
Jeffrey Liu <gpugcc@gmail.com>
2024-05-14 23:59:25 +0800

    Revert "Merge branch 'dev_內網版本' into main_內網版本"
    This reverts commit b18400b55c63c4c37cf50ffcd9abe735becfaa27, reversing
    changes made to d1e79d64661e04699230ccb60fe6487098b0e772.

--- src/modules/home.ts
+++ src/modules/home.ts
@@ -86,7 +86,7 @@ function CheckGuest({ account, password, err }) {
 }
 
 export { ValidateRules } from './validate';
-export { Home, Register, Logout } from '@/modules/common';
+export { Register, Logout } from '@/modules/common';
 export {
     Form,
     Login,
